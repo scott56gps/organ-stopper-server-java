3cd0a18930b0fcfe59b18a7296757e7a1d62942a
Scott Nicholes <scott93gps@gmail.com>
2024-06-11 01:30:03 -0700

    Added tests and bounds checks

--- src/main/java/com/scott/organ_stopper_server/service/OrganService.java
+++ src/main/java/com/scott/organ_stopper_server/service/OrganService.java
@@ -3,27 +3,39 @@ package com.scott.organ_stopper_server.service;
 import com.scott.organ_stopper_server.model.Organ;
 import org.springframework.stereotype.Service;
 
-import java.util.Comparator;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class OrganService {
-    private static final HashMap<Integer, Organ> organs = new HashMap<>();
+    private static List<Organ> organs = new ArrayList<>();
 
-    public OrganService() {
-        organs.put(0, new Organ("Walt Disney Concert Hall"));
-        organs.put(1, new Organ("Richland Stake Center"));
+    public OrganService() {}
+
+    public OrganService(List<String> organNames) {
+        organs = organNames.stream()
+                .map(Organ::new)
+                .collect(Collectors.toList());
     }
 
     public Organ getOrgan(int index) {
-        if (!organs.containsKey(index)) {
-            String errorMessage = "Provided index: " + index + " is not a valid index to retrieve an organ.  Please provide an index ";
-            errorMessage += organs.keySet().stream().max(Comparator.naturalOrder())
-                    .map(maxKey -> "between 0 and " + maxKey)
-                    .orElse("greater than 0");
+        if (organs.isEmpty()) {
+            throw new RuntimeException("There are no organs.  Please create an organ.");
+        }
+        if (index < 0) {
+            throw new IllegalArgumentException("Cannot retrieve organ for index less than 0.");
+        }
+        if (index >= organs.size()) {
+            String errorMessage = "Provided index: " + index + " is out of range.  " +
+                    "Please provide an index between 0 and " + (organs.size() - 1);
             throw new IllegalArgumentException(errorMessage);
         }
 
         return organs.get(index);
+    }
+
+    public void appendOrganNames(List<String> organNames) {
+        organs.addAll(organNames.stream().map(Organ::new).toList());
     }
 }
